ccd3c451f65e9aa6f7d8a0586ae315c8da050f8c
Mathias Ayuba <mathiayuba@gmail.com>
2026-08-13 10:31:29 +0100

    refactor: move server configuration and proxy fix setup to improve app initialization

--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,15 @@ from flask_cors import CORS
 from extension import bcrypt, jwt, oauth
 from auth import auth_bp
 from config import Config
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 CORS(app, origins=[Config.FRONTEND_URL], supports_credentials=True)
 app.config.from_object(Config)
+
+app.config["SERVER_NAME"] = Config.SERVER_NAME
+app.config["PREFERRED_URL_SCHEME"] = Config.PREFERRED_URL_SCHEME
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1)
 
 app.config["BREVO_API_KEY"] = Config.BREVO_API_KEY
 app.config["MAIL_FROM"] = Config.MAIL_FROM
