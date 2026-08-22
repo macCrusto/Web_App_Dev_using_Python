6ae9e53313d9dc497d15c3bc780d1d16ed509526
Mathias Ayuba <mathiayuba@gmail.com>
2026-08-22 20:58:15 +0100

    fix: replace anchor tags with divs for logo display in multiple pages

--- frontend/src/pages/ForgotPassword/ForgotPassword.tsx
+++ frontend/src/pages/ForgotPassword/ForgotPassword.tsx
@@ -8,10 +8,12 @@ export default function ForgotPasswordPage() {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center dark:bg-zinc-950">
       <div className="w-full max-w-md flex flex-col items-center justify-center gap-4 scale-85">
-        <a href="#" className="flex items-center justify-center gap-2 font-semibold text-zinc-900 dark:text-zinc-50">
-          <img src={logo} className="size-8" />
+        <div
+          className='flex items-center justify-center gap-2 font-semibold text-zinc-900 dark:text-zinc-50'
+        >
+          <img src={logo} className='size-8' />
           Petur
-        </a>
+        </div>
         <ForgotPasswordForm 
           className="w-sm" 
           onSubmit={handleSubmit}
