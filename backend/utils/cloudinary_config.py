6183ab8f76525f5c97a4a60cf6b525ef5d4e18ca
Mathias Ayuba <mathiayuba@gmail.com>
2026-09-08 13:18:58 +0100

    feat: add cloudinary configuration for image management

---
+++ backend/utils/cloudinary_config.py
@@ -0,0 +1,8 @@
+import cloudinary
+from config import Config
+cloudinary.config(
+    cloud_name=Config.CLOUDINARY_CLOUD_NAME,
+    api_key=Config.CLOUDINARY_API_KEY,
+    api_secret=Config.CLOUDINARY_API_SECRET,
+    secure=True
+)
